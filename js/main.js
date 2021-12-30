c82f0d9acc6b5166ddce9e57c12baa0517efe803
Yu4iy <yurimohyliuk@gmail.com>
2021-12-30 12:24:13 +0100

    ADD: Video Logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
 const openModal = document.querySelector('.open-modal')
-const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.close')
-const video = document.querySelector('.video')
+const modal = document.querySelector('.modal')
 
 openModal.addEventListener('click', ()=>{
 	modal.classList.add('active')
+	player.playVideo()
 })
 
 closeModal.addEventListener('click', ()=>{
 	modal.classList.remove('active')
-		
+	stopVideo()
 })
+
+
+
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('video', {
+    height: '660',
+    width: '1040',
+    videoId: 'G1IbRujko-A',
+
+  });
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
